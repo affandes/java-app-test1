e6cd8decf52408f7d9606bf8cda951c46116c197
Muhammad Affandes <affandes@uin-suska.ac.id>
2024-03-08 08:32:13 +0700

    Initial commit

---
+++ src/main/java/com/github/affandes/belajar/javaapptest1/JavaAppTest1Application.java
@@ -0,0 +1,13 @@
+package com.github.affandes.belajar.javaapptest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAppTest1Application.class, args);
+    }
+
+}
